b38e5d653f95a3ae711ab2fe3f2cb98c09b95718
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 19:21:15 -0400

    hit boxes

--- assets/scenes/map/monster_2d_follow.gd
+++ assets/scenes/map/monster_2d_follow.gd
@@ -1,13 +1,11 @@
 extends PathFollow2D
 
 @export var monster: Node2D = null
-@onready var sprite_2d = $Sprite2D
-
-@onready var path_follow = get_parent()
 
 @export var speed = 20.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(monster._visual_resource)
-	sprite_2d.texture = monster._visual_resource
+	var path_follow_area_2d = monster.get_path_follow_area_2d()
+	monster.remove_child(path_follow_area_2d)
+	add_child(path_follow_area_2d)
 	loop = false
--- assets/scenes/npcs/monster.gd
+++ assets/scenes/npcs/monster.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@export var monster_name: String = ""
 @export var _speed: float = 0.0
 @export var _max_souls_to_consume: int = 1
 @export var _visual_resource: Resource
 @export var puzzles: Array[Node2D] = []
+
+@onready var path_follow_area_2d = $PathFollowArea2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +18,9 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func get_random_puzzle() -> Node2D:
 	return puzzles.pick_random()
+
+func get_path_follow_area_2d() -> Area2D:
+	assert(path_follow_area_2d != null, str("PathFollowArea2D must be set in ", monster_name, " scene"))
+	return path_follow_area_2d
