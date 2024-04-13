96a15bb07b9e7e6758a6b1f74a47f99efe069b98
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 01:12:28 -0400

    better shit

--- assets/scenes/maps/map_path.gd
+++ assets/scenes/maps/map_path.gd
@@ -1,17 +1,12 @@
 extends Path2D
 
-const BASIC_HUMAN_SCENE = preload("res://assets/scenes/Humans/basic_human.tscn");
+const HUMAN_SCENE = preload("res://assets/scenes/Patrons/human.tscn");
 
 
 
 func add_human():
-	var new_human = load("res://assets/scenes/Humans/basic_human.tscn").instantiate()
+	var new_human = HUMAN_SCENE.instantiate()
 	add_child(new_human)
-	# Set human's position to the first point of the path
-	#if curve.get_point_count() > 0:
-		#new_human.position = curve.get_point_position(0)
-
 
 func _on_timer_timeout():
-	print('hi')
 	add_human()
