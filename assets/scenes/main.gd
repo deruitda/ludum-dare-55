8a08d71dca17feb02f472d08df9add952f479e84
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-13 01:09:38 -0400

    added main and level nodes with base patronSurvived functionality

--- assets/scenes/main.gd
+++ assets/scenes/main.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var current_level = 1
+@onready var status = "playing"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func gameOver():
+	status = "gameOver"
