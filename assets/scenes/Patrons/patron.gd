6252ac0894e2002f5429c7d67e3ab8038e051d83
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 14:59:43 -0400

    fixing human

--- assets/scenes/Patrons/patron.gd
+++ assets/scenes/Patrons/patron.gd
@@ -1,28 +1,7 @@
-extends PathFollow2D
-
-
-@onready var path_follow = get_parent()
+extends Node2D
 
 @export var souls = 1
 @export var health = 1
 @export var speed = 100.0
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	loop = false
 
-func _physics_process(delta):
-	set_progress(get_progress() + (speed * delta))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if get_progress_ratio() == 1.0:
-		patron_survived()
-		
-func patron_survived():
-	SurviveSignal.emit_signal("patron_survived", self)
-	queue_free()
-	
-func patron_died():
-	DeathSignal.emit_signal("patron_died", self)
-	
+@export var _visual_resource: Resource
