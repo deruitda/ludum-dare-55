9bf413a0b0c9d57dddd8da9f068ce125d93d66c6
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-13 09:14:07 -0400

    add current_soul_count to game state

--- assets/scenes/game_state.gd
+++ assets/scenes/game_state.gd
@@ -1,7 +1,9 @@
 extends Node
 
-var souls_captured: int = 0
-var souls_survived: int = 0
+var souls_captured: int = 0 # Total number of souls captured throughout level
+var current_soul_count: int = 0 # Number of souls able to spend on summoning monsters
+var souls_survived: int = 0 # If too many souls survive then it's game over
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SurviveSignal.connect("patron_survived", on_patron_survived)
@@ -13,6 +15,7 @@ func on_patron_survived(patron):
 	
 func on_patron_died(patron):
 	souls_captured += patron.souls
+	current_soul_count += patron.souls
 	
 func set_level(level):
 	reset_souls()
@@ -20,6 +23,7 @@ func set_level(level):
 func reset_souls():
 	souls_captured = 0
 	souls_survived = 0
+	current_soul_count = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
