780fc2e9b540626c75bc818e8053beac70ea757e
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 11:04:50 -0400

    setting up input action

--- assets/scenes/monsters/monster_card.gd
+++ assets/scenes/monsters/monster_card.gd
@@ -1,6 +1,7 @@
 extends Node
 const Monster = preload("res://assets/scenes/monsters/monster.gd")
 @export var monster: Node2D = null
+@export var summon_monster_input_action_number: int = 1
 @onready var sprite_2d = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -9,6 +10,30 @@ func _ready():
 	pass # Replace with function body.
 
 
+		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	check_input_action()
+	pass
+	
+func check_input_action():
+	match summon_monster_input_action_number:
+		1:
+			if Input.is_action_pressed("select_monster_1"):
+				begin_summoning_monster()
+		2:
+			if Input.is_action_pressed("select_monster_2"):
+				begin_summoning_monster()
+		3:
+			if Input.is_action_pressed("select_monster_3"):
+				begin_summoning_monster()
+		
+	
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		begin_summoning_monster()
+		
+
+func begin_summoning_monster():
 	pass
