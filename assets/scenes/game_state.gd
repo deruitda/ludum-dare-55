2d79fbf6c051881309a5eed66cd56720131e4d06
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 02:35:39 -0400

    game state

---
+++ assets/scenes/game_state.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var souls_captured: int = 0
+var souls_survived: int = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SurviveSignal.connect("patron_survived", on_patron_survived)
+	DeathSignal.connect("on_patron_died", on_patron_died)
+
+func on_patron_survived(patron):
+	souls_survived += patron.souls
+	
+func on_patron_died(patron):
+	souls_captured += patron.souls
+	
+func set_level(level):
+	reset_souls()
+
+func reset_souls():
+	souls_captured = 0
+	souls_survived = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- assets/scenes/main.gd
+++ assets/scenes/main.gd
@@ -5,20 +5,17 @@ extends Node2D
 @onready var current_level_index = 0
 @onready var status = "playing"
 
+
 const LEVELS = [
 	"res://assets/scenes/levels/level_1.tscn"
 	
 ]
 func _ready():
-	SurviveSignal.connect("patron_survived", on_patron_survived)
 	var scene = load(LEVELS[current_level_index])
 	current_level.get_tree().change_scene_to_packed(scene)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func on_patron_survived(patron):
-	var souls_captured = patron.souls
 
 func gameOver():
 	status = "gameOver"
