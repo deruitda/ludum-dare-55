257e6fc7a99ca74d87f2bd14e3d06f088aa50fe3
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 21:43:14 -0400

    adding collisions

--- assets/scenes/npcs/patron.gd
+++ assets/scenes/npcs/patron.gd
@@ -6,8 +6,12 @@ extends Node2D
 @export var speed = 100.0
 
 @export var _visual_resource: Resource
-@onready var path_follow_area_2d = $PathFollowArea2D
 
 func get_path_follow_area_2d() -> Area2D:
+	var path_follow_area_2d = get_node("PathFollowArea2D")
 	assert(path_follow_area_2d != null, str("PathFollowArea2D must be set in ", patron_class, " scene"))
 	return path_follow_area_2d
+
+
+func _on_path_follow_area_2d_area_entered(area):
+	print("touched")
