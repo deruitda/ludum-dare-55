d1a6c5097340f8cddf24d620a9b5d54fb324ab0f
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 21:16:10 -0400

    wip

--- assets/scenes/map/patron_2d_follow.gd
+++ assets/scenes/map/patron_2d_follow.gd
@@ -1,14 +1,9 @@
 extends PathFollow2D
 
 @export var patron: Node2D = null
-@onready var sprite_2d = $Sprite2D
-
-@onready var path_follow = get_parent()
-
 @export var speed = 100.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite_2d.texture = patron._visual_resource
 	loop = false
 
 func _physics_process(delta):
@@ -27,3 +22,9 @@ func patron_survived():
 func patron_died():
 	SoulsCapturedSignal.emit_signal("patron_died", patron)
 	
+func add_patron(new_patron):
+	patron = new_patron
+	var path_follow_area_2d = patron.get_path_follow_area_2d()
+	print (patron.path_follow_area_2d)
+	patron.remove_child(path_follow_area_2d)
+	add_child(path_follow_area_2d)
--- assets/scenes/npcs/monster.gd
+++ assets/scenes/npcs/monster.gd
@@ -5,14 +5,12 @@ extends Node2D
 @export var _max_souls_to_consume: int = 1
 @export var _visual_resource: Resource
 @export var puzzles: Array[Node2D] = []
-
 @onready var path_follow_area_2d = $PathFollowArea2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#loop = false
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
