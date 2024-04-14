33c68c7fa85edef45c946e1fda44fa649b8f62cf
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-13 21:42:43 -0400

    making patron cleaner

--- assets/scenes/map/patron_2d_follow.gd
+++ assets/scenes/map/patron_2d_follow.gd
@@ -4,7 +4,7 @@ extends PathFollow2D
 @export var speed = 100.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var path_follow_area_2d = patron.get_node("PathFollowArea2D")
+	var path_follow_area_2d = patron.get_path_follow_area_2d()
 	patron.remove_child(path_follow_area_2d)
 	add_child(path_follow_area_2d)
 	loop = false
