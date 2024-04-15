105cb1355d42a0a3df48edc47d6619e05adb9151
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-14 22:42:58 -0400

    adding select noise

--- assets/scenes/monsters/monster_card.gd
+++ assets/scenes/monsters/monster_card.gd
@@ -1,16 +1,18 @@
-extends Button
+extends TextureButton
+
 
 const Monster = preload("res://assets/scenes/npcs/monster.gd")
-
-@export var monster: Node2D = Monster.new()
+@export var monster: Node2D = null
 @export var summon_monster_input_action_number: int = 1
+@onready var select_sound = $SelectSound
 
-@onready var audio_stream_player = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SummoningSignal.connect("state_changed", _on_state_changed)
+	pass
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	check_input_action()
 
@@ -26,23 +28,14 @@ func check_input_action():
 			if Input.is_action_just_pressed("select_monster_3"):
 				begin_summoning_monster()
 
+
+
 func begin_summoning_monster():
 	$BorderActive.visible = true
 	SummoningSignal.emit_signal("monster_selected", monster)
 
-func end_summoning_monster():
-	$BorderActive.visible = false
-
 
 func _on_pressed():
-	audio_stream_player.play()
-	if SummoningState.current_state == SummoningState.summoning_states.IDLE:
-		print("Beginnin summon monster")
-		begin_summoning_monster()
-	else:
-		print("Ending summon monster")
-		end_summoning_monster()
+	select_sound.play()
+	begin_summoning_monster()
 	
-func _on_state_changed(new_state):
-	if new_state == SummoningState.summoning_states.IDLE:
-		end_summoning_monster()
