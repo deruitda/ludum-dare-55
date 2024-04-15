e92b35dcaa76fa2c8443ad8f316ceb3d320e92f9
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-15 01:13:39 -0400

    game over functionality

--- assets/scenes/Hud/hud.gd
+++ assets/scenes/Hud/hud.gd
@@ -1,23 +1,24 @@
 extends Control
 
+
 var play_button_texture = preload("res://assets/sprites/misc/icons/play.png")
 var pause_button_texture = preload("res://assets/sprites/misc/icons/pause.png")
+
 
 @onready var health_bar = %HealthBar
 @onready var health_bar_text = %PatronsSurvivedLabel
 @onready var souls_text = %Souls
 @onready var pause_button = %PauseButton
-@onready var pause_menu = %PauseMenu
 
-@export var _total_patrons_allowed_to_survive = 100
 
 func _ready(): 
 	SurviveSignal.connect("patron_survived_updated", updated_patron_survived_hud)
 	SoulsCapturedSignal.connect("souls_captured_updated", _on_souls_captured_updated)
+	health_bar_text.text = str(GameState.remaining_patrons_allowed_to_survive) + " Survivors Remaining"
 
 
 func updated_patron_survived_hud():
-	health_bar.value = 100 * GameState.remaining_patrons_allowed_to_survive / GameState.total_patrons_allowed_to_survive
+	health_bar.value = 100 * GameState.remaining_patrons_allowed_to_survive / GameState._total_patrons_allowed_to_survive
 	health_bar_text.text = str(GameState.remaining_patrons_allowed_to_survive) + " Survivors Remaining"
 
 
@@ -30,10 +31,8 @@ func _on_pause_button_pressed():
 	if GameState.is_paused:
 		# Game is paused, resume the game
 		GameSignal.emit_signal("game_resumed")
-		pause_menu.visible = false
 		pause_button.texture_normal = pause_button_texture
 	else:
 		# Game is playing, pause the game
 		GameSignal.emit_signal("game_paused")
-		pause_menu.visible = true
 		pause_button.texture_normal = play_button_texture
--- assets/scenes/main.gd
+++ assets/scenes/main.gd
@@ -1,27 +1,22 @@
 extends Node2D
 
-@onready var current_level = %CurrentLevel
-@onready var pause_menu = %PauseMenu
 
+@onready var current_level = %CurrentLevel
 @onready var current_level_index = 0
-@onready var status = "playing"
 
 
 const LEVELS = [
 	"res://assets/scenes/levels/level_1.tscn"
 ]
 
+
 func _ready():
 	GameSignal.connect("game_paused_updated", _on_game_paused_updated)
+	GameSignal.connect("game_over", _on_game_over)
+	GameSignal.connect("game_restarted", _on_game_restarted)
 	var scene = load(LEVELS[current_level_index]).instantiate()
 	current_level.add_child(scene)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func gameOver():
-	status = "gameOver"
 	
 func _on_game_paused_updated():
 	if GameState.is_paused:
@@ -30,5 +25,9 @@ func _on_game_paused_updated():
 		get_tree().paused = false
 
 
-func _on_play_button_pressed():
-	GameSignal.emit_signal("game_resumed")
+func _on_game_over():
+	get_tree().paused = true
+
+
+func _on_game_restarted():
+	get_tree().paused = false
