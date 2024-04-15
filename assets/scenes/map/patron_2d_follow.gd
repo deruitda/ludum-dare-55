870f9fd53644c755925410d034011931b1859807
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-14 23:48:34 -0400

    animations

--- assets/scenes/map/patron_2d_follow.gd
+++ assets/scenes/map/patron_2d_follow.gd
@@ -7,7 +7,8 @@ func _ready():
 	loop = false
 
 func _physics_process(delta):
-	set_progress(get_progress() + (patron.speed * delta))
+	if !patron.currently_dying:
+		set_progress(get_progress() + (patron.speed * delta))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,6 +21,3 @@ func patron_survived():
 	
 func patron_died():
 	SoulsCapturedSignal.emit_signal("patron_died", patron)
-
-func _on_path_follow_area_2d_area_entered(area):
-	pass # Replace with function body.
--- assets/scenes/npcs/patron.gd
+++ assets/scenes/npcs/patron.gd
@@ -1,21 +1,22 @@
 extends Node2D
 
-@export var patron_class: String = ""
 @export var souls = 1
 @export var _total_health = 1
 @export var speed = 100.0
 
-@export var _visual_resource: Resource
-
 @onready var health: int
-@onready var patron_path_follow_2d = $".."
+@onready var audio_stream_player = $AudioStreamPlayer
+
+@onready var animated_sprite_2d = $PathFollowArea2D/AnimatedSprite2D
+@onready var death_sound_container = $DeathSoundContainer
+@onready var currently_dying: bool = false
 
 func _ready():
 	health = _total_health
 	
 func _process(delta):
 	if health <= 0:
-		die()
+		trigger_death()
 
 func _on_path_follow_area_2d_area_entered(monster_area):
 	var monster = monster_area.get_parent()
@@ -30,5 +31,16 @@ func capture_souls(souls_to_capture: int)-> int:
 	health -= total_damage
 	return total_damage
 
-func die():
-	patron_path_follow_2d.queue_free()
+func trigger_death():
+	currently_dying = true
+	animated_sprite_2d.play("dying")
+	audio_stream_player.play()
+	
+
+func destroy():
+	get_parent().queue_free()
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite_2d.animation == "dying":
+		destroy()
+
