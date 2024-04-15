6e7f61f406393c503d23cb976d8567da25ca1151
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-15 03:22:37 -0400

    add resume button to pause menu and add blank options pause menu

--- assets/scenes/pause_menu/pause_menu.gd
+++ assets/scenes/pause_menu/pause_menu.gd
@@ -1,25 +1,38 @@
 extends Control
 
 
+@onready var main_pause_menu = %MainPauseMenu
+@onready var options_pause_menu = %OptionsPauseMenu
+
+
+func _on_resume_pressed():
+	GameSignal.emit_signal("game_resumed")
+
+
 func _on_restart_pressed():
 	GameSignal.emit_signal("game_restarted")
+
+
+func _on_options_button_pressed():
+	main_pause_menu.visible = false
+	options_pause_menu.visible = true
 
 
 func _on_quit_button_pressed():
 	GameSignal.emit_signal("game_quit")
 
 
+func _on_back_button_pressed():
+	options_pause_menu.visible = false
+	main_pause_menu.visible = true
+
+
 func _input(event):
-	print("input event in pause menu")
 	if event.is_action_pressed("esc"):
-		print("esc hit")
 		if GameState.is_game_over:
-			print("game is over, cannot resume")
 			pass
 		elif GameState.is_paused:
-			print("resuming game")
 			GameSignal.emit_signal("game_resumed")
 		elif SummoningState.current_state == SummoningState.summoning_states.IDLE:
-			print("pausing game")
 			GameSignal.emit_signal("game_paused")
 		get_tree().get_root().set_input_as_handled()
