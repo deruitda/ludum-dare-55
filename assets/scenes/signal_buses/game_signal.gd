caecb6c09a2e97399fc61329658cfc7b73055a9f
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-15 10:47:38 -0400

    created main menu

--- assets/scenes/signal_buses/game_signal.gd
+++ assets/scenes/signal_buses/game_signal.gd
@@ -1,9 +1,10 @@
 extends Node
 
 
-signal game_paused()
-signal game_resumed()
-signal game_paused_updated()
 signal game_over()
+signal game_paused()
+signal game_paused_updated()
 signal game_quit()
 signal game_restarted()
+signal game_resumed()
+signal new_game()
