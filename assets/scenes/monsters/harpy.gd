cc615b3c2c3503e1782c218ed8e3d1625f8c8c2e
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-04-15 02:47:38 -0400

    adding harpys

--- assets/scenes/monsters/harpy.gd
+++ assets/scenes/monsters/harpy.gd
@@ -1,25 +1,14 @@
 extends "res://assets/scenes/npcs/monster.gd"
-
-@onready var is_running: bool = false
-@onready var running_direction: Vector2
-
-func set_running_direction(direction: Vector2):
-	running_direction = direction
+@onready var attack_node = $AttackNode
 
 func _on_summoning_animation_finished():
-	monster_animation.play("charging_up")
-
-func _on_charging_up_animation_finished():
-	monster_animation.play("idle")
-	is_running = true
-
-func _on_animated_sprite_2d_animation_finished():
 	super()
-	if monster_animation.animation == "charging_up":
-		_on_charging_up_animation_finished()
+	attack_node.start_attacking()
 
-func _physics_process(delta):
-	super(delta)
-	if is_running:
-		var velocity = running_direction * _speed * delta
-		position += velocity
+
+func _on_attack_node_attack():
+	monster_animation.play("using_power")
+
+
+func _on_attack_node_attack_finished():
+	monster_animation.play("idle")
