8c4e2c973d818d32553121f7a90d507df3fc3ad3
Nick Mikelsavage <nick@njsmdigital.com>
2024-04-15 16:57:01 -0400

    spawn monsters while other animation is playing

--- assets/scenes/summoning_state.gd
+++ assets/scenes/summoning_state.gd
@@ -68,7 +68,6 @@ func get_summoning_monster_path_follow_2d():
 
 func _on_monster_summoned(_monster):
 	reset_state()
-	
 
 
 func _on_monster_summoned_canceled():
@@ -110,6 +109,3 @@ func _on_incantation_typing():
 func _on_incantation_stopped_typing():
 	is_incantation_typing = false
 	SummoningSignal.emit_signal("incantation_typing_updated")
-
-func _on_summoning_animation_finished():
-	set_state(summoning_states.IDLE)
